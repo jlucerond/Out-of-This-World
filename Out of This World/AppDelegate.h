bbfb5bbae455482020eb45fea15677dad0b9c081
Joe Lucero <jlucerond@gmail.com>
2015-07-08 10:43:44 -0400

    Initial Commit

---
+++ Out of This World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of This World
+//
+//  Created by Joe Lucero on 7/8/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of This World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of This World
+//
+//  Created by Joe Lucero on 7/8/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
